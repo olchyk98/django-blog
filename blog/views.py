664b124636280a47735064ef68cff95c18245d20
Oles Odynets <40524044+olchyk98@users.noreply.github.com>
2019-03-11 22:13:31 +0100

    75% server register POSTR

--- blog/views.py
+++ blog/views.py
@@ -3,43 +3,71 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import View
+from django.http import HttpResponse
 
 import json
 
-from .models import Post
+from .models import Post, Author
 
 # Create your views here.
 
 class LoginView(View):
-    def get(self, req):
-        return render(req, 'blog/login.html', context = {
+    def get(self, request):
+        return render(request, 'blog/login.html', context = {
 
         })
     # end
 # end
 
 class RegisterView(View):
-    def get(self, req):
-        return render(req, 'blog/register.html', context = {
+    def get(self, request):
+        return render(request, 'blog/register.html', context = {
 
         })
     # end
+
+    def post(self, request):
+        # Get body
+        data = json.loads(request.body)
+        resolved = False
+
+        if(not request.user): request.user = {}
+
+        def submit(status):
+            resolved = True
+            return HttpResponse(json.dumps({
+                'status': status
+            }))
+
+        # Check if user with login exists
+        if(Author.objects.get(login = data['login'])):
+            return submit(400)
+
+        # Create user
+        user = Author()
+        user.login = request.user['login'] = data['login']
+        user.password = request.user['password'] = data['password']
+        user.name = data['name']
+        user.save()
+
+        # Submit success response
+        if(not resolved): return submit(200)
 # end
 
 class PostsList(View):    
-    def get(self, req):
+    def get(self, request):
         # Get posts
         posts = Post.objects.all()
 
         # Render posts
-        return render(req, 'blog/posts_list.html', context = {
+        return render(request, 'blog/posts_list.html', context = {
             'posts': posts
         })
     # end
 # end
 
 class ViewPost(View):
-    def get(self, req, id):
+    def get(self, request, id):
         post = get_object_or_404(Post, id = id)
 
         comments = list(post.comments.all())
@@ -50,7 +78,7 @@ class ViewPost(View):
             comments[ma].likesInt = len(likes)
             comments[ma].isLiked = "1" in likes
 
-        return render(req, 'blog/post.html', context = {
+        return render(request, 'blog/post.html', context = {
             'post': post,
             'comments': comments
         })
--- static/blog/register/script.js
+++ static/blog/register/script.js
@@ -18,5 +18,31 @@ window.onload = function() {
     document.getElementById("register-form").addEventListener('submit', e => {
         e.preventDefault();
         console.log("REGISTER");
+        fetch(`${ window._HOSTPATH_ }/register/`, {
+            method: "POST",
+            headers: {
+                "X-CSRFToken": Cookies.get("csrftoken")
+            },
+            body: JSON.stringify({
+                login: fields.login,
+                name: fields.name,
+                password: fields.password
+            })
+        }).then((res) => {
+            if(res.ok) {
+                return res.json();
+            } else {
+                throw new Error(`${ res.status } (${ res.statusText })`)
+            }
+        }).then((e) => {
+            switch(e.status) {
+                case 400: // ERROR: user exists
+                    console.log("ERROR");
+                break;
+                case 200: // SUCCESS: registered
+                    console.log("SUCCESS");
+                break;
+            }
+        }).catch(console.error);
     });
 }
